5a0ddd70b78cd671ace0caa9307588892756b46b
Hasan Demirtaş <utsukushihito@outlook.com>
2024-07-29 15:04:22 +0300

    add resolver. (#128)

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,3 +1,5 @@
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") }
+
 dependencyResolutionManagement {
     versionCatalogs { create("libs") { from(files("../gradle/libs.versions.toml")) } }
 }
